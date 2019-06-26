4090b486e60f1f55b4d3b76ae2b5b30b8498db1f
chixiaocheng <18778525819@163.com>
2019-06-26 21:15:20 +0800

    0.0.2.0
    引入第三方UI：colorui

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 //app.js
 App({
-  onLaunch: function () {
-    
-  },
-  data: {
-    
-  }
+    // 注册内容
+    onLaunch: function() {
+        // 提示等待
+        wx.showLoading({
+            title: 'Loading',
+        })
+
+        // 获取系统状态栏信息（ColorUI需要）
+        wx.getSystemInfo({
+            success: e => {
+                console.log(e)
+                this.globalData.StatusBar = e.statusBarHeight;
+                let custom = wx.getMenuButtonBoundingClientRect();
+                this.globalData.Custom = custom;
+                let extra = (e.system.substring(0, 3) == 'iOS') ? e.statusBarHeight : 0; //单独适配iOS系统
+                this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight + extra;
+            }
+        })
+    },
+
+    // 全局数据
+    globalData: {
+        site: 'https://amateur.wx243.com', //服务器
+        ikey: wx.getStorageSync('ikey'), //身份秘钥
+    }
 });

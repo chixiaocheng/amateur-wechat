9db4b84a69864e0fbae2588aa6314abf36a4c948
chixiaocheng <18778525819@163.com>
2019-06-26 21:59:50 +0800

    0.0.2.1

---
+++ utils/copy.js
@@ -0,0 +1,17 @@
+
+const app = getApp();
+const f = require("../../../utils/function.js"); //自定义函数
+
+Page({
+
+    data: {
+
+    },
+
+    onLoad: function (options) {
+        f.GET({
+            
+        })
+    },
+
+})
---
+++ utils/function.js
@@ -0,0 +1,52 @@
+// /utils/function.js
+const app = getApp();
+
+// 自定义GET类型请求
+function GET(e) {
+    let module = (e.module) ? e.module : 'index';
+    let url = app.globalData.site + '/' + module + '.php?tt=wechat&m=' + e.model + '&ikey=' + app.globalData.ikey;
+    wx.request({
+        url: url,
+        data: e.data,
+        header: {},
+        method: 'GET',
+        dataType: 'json',
+        responseType: 'text',
+        success: function(res) {
+            e.success;
+            console.log(url)//TODO
+        },
+        fail: e.fail,
+        complete: e.complete
+    })
+}
+
+// 自定义POST类型请求
+// function POST(e) {
+//     var showingPath = getCurrentPages()[getCurrentPages().length - 1].route;
+//     var url = app.site + '/api.php?path=' + showingPath + '&ikey=' + app.ikey;
+//     Object.keys(e.GETdata).forEach((key) => {
+//         url += '&' + key + '=' + e.GETdata[key];
+//     });
+//     wx.request({
+//         url: url,
+//         data: e.POSTdata,
+//         header: {
+//             'Content-Type': 'application/x-www-form-urlencoded'
+//         },
+//         method: 'POST',
+//         dataType: 'json',
+//         responseType: 'text',
+//         success: function(res) {
+//             checkData(res, e.success)
+//         },
+//         fail: e.fail,
+//         complete: e.complete
+//     })
+// }
+
+// 函数输出
+module.exports = {
+    GET: GET,
+    //POST: POST,
+};
